33d8251c65f9fbd18c4501dcffb73ba4c9ec200f
吴衡 <wuheng@zdns.cn>
2024-06-19 10:39:07 +0800

    wip

--- cmd/gobco-tool/gobco-tool.go
+++ cmd/gobco-tool/gobco-tool.go
@@ -6,13 +6,16 @@ import (
 	"github.com/junhwi/gobco/instrument"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"os/exec"
 	"path"
 )
 
 func getFd(out string) (*os.File, error) {
 	if out == "" {
-		return os.Stdout, nil
+		worker := os.Getenv("WORK")
+		return os.Create(filepath.Join(worker, "b100", "gobco.cover.go"))
 	} else {
 		return os.Create(out)
 	}
@@ -25,6 +28,8 @@ func runGobco() {
 	outPtr := cmd.String("o", "", "file for output; default: stdout")
 	version := cmd.String("V", "", "print version and exit")
 	cmd.String("mode", "", "coverage mode: set, count, atomic")
+	cmd.String("pkgcfg", "", "coverage pkgcfg")
+	cmd.String("outfilelist", "", "coverage outfilelist")
 	coverVar := cmd.String("var", "Cov", "name of coverage variable to generate (default \"Cov\")")
 	cmd.Parse(os.Args[2:])
 	files := cmd.Args()
@@ -40,6 +45,16 @@ func runGobco() {
 	}
 }
 
+func fixArgs(args []string) []string {
+	var newArgs  []string
+	for _, arg := range args  {
+		if !strings.HasPrefix(arg ,"-coveragecfg=") {
+			newArgs = append(newArgs, arg)
+		}
+	}
+	return newArgs
+}
+
 func main() {
 	log.SetFlags(0)
 	log.SetPrefix("gobco: ")
@@ -51,6 +66,10 @@ func main() {
 	if toolName == "cover" {
 		runGobco()
 	} else {
+		args = fixArgs(args)
+		file, _ := os.OpenFile("example.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		fmt.Fprintf(file, "args: %+v\n", args)
+		defer file.Close()
 		cmd := exec.Command(tool, args...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
